72fd52daaf116269c4af7892bd4fd53d315b787e
Luminda Saranga <sluminda@gmail.com>
2024-05-23 22:25:22 +0530

    sass Added Project Cleared

--- JS/header.js
+++
@@ -1,9 +0,0 @@
-const wideSearchForm = document.querySelector(
-  ".wide-header-items-container .wide-search-form"
-);
-const wideSearchInput = document.querySelector(".wide-search-form input");
-
-wideSearchForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  wideSearchInput.focus();
-});
